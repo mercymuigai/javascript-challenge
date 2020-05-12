17b2c385e21296deaa9f8420732359412acf744f
mercymuigai <muigai.mercy@gmail.com>
2020-05-12 15:36:59 -0500

    edited script and html file

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -13,18 +13,10 @@ console.log(data);
 // update each cell's text with ufo report  values (date, city, state, country,shape,duration, comments)
 
 data.forEach((ufoReport) => {
-    //{datetime: "1/1/2010",city: "benton",state: "ar",country: 
-    // "us",shape: "circle",durationMinutes: "5 mins.",comments:
-    //  "4 bright green...."
+    
       var row = tbody.append("tr");
       Object.entries(ufoReport).forEach(([key,value]) => {
-        // datetime: "1/1/2010",
-        // city: "benton",
-        // state: "ar",
-        // country: "us",
-        // shape: "circle",
-        // durationMinutes: "5 mins.",
-        // comments: "4 bright green...."
+        
         var cell =row.append("td");
 
         // incase of blanks in the report
@@ -36,22 +28,6 @@ data.forEach((ufoReport) => {
         }
       });
   });
-
-// //  Loop Through `data` and console.log each ufo report object
-// // append one table row `tr` for each ufo report object
-// //Use `Object.entries` to console.log each weather report value
-// //append 1 cell per ufo report value (date, city, state, country,shape,duration, comments)
-// // update each cell's text with ufo report  values (date, city, state, country,shape,duration, comments)
-// data.forEach(function(ufoReport) {
-//     var row = tbody.append("tr");
-//     Object.entries(ufoReport).forEach(function([key, value]) {
-//       console.log(key, value);
-//       // Append a cell to the row for each value
-//       // in the ufo report object
-//       var cell = row.append("td");
-//       cell.text(value);
-//     });
-//   });
 
 // Select the button
 var button = d3.select("#filter-btn");
@@ -70,6 +46,28 @@ button.on("click", function() {
   var filteredDate = data.filter(row => row.datetime === inputValue);
 
   console.log(filteredDate);
-});
 
+
+ // script to clear the data to avoid appending at the bottom
+tbody.html("");
+
+// This section is so that the script that loops through the selected dates of the data
+
+  filteredDate.forEach((ufoReport) => {
+    console.log(ufoReport);
+  var row = tbody.append("tr");
+      Object.entries(ufoReport).forEach(([key,value]) => {
+        
+        var cell =row.append("td");
+
+        // incase of blanks in the report
+        if (value.length === 0){
+            cell.text("NA");
+        }
+        else {
+            cell.text(value);
+        }
+      });
+});
+});
 
